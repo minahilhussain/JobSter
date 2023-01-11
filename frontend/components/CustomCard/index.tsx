c73462d9e06608774e79ef7f4f6431991afc1bd9
minahilhussain <minahilh21@gmail.com>
2023-01-11 11:59:31 +0500

    added icons in job card

--- frontend/components/CustomCard/index.tsx
+++ frontend/components/CustomCard/index.tsx
@@ -2,6 +2,10 @@ import moment from 'moment';
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { StyledHeader, StyledButton, StyledCard, StyledTitle } from './styles';
+import { FaBlackTie } from 'react-icons/fa';
+import { MdLocationOn } from 'react-icons/md';
+import { BsFillCalendarDayFill } from 'react-icons/bs';
+
 const CustomCard = ({ job }: any) => {
   return (
     <StyledCard key={job?.id}>
@@ -9,8 +13,11 @@ const CustomCard = ({ job }: any) => {
       <Card.Body>
         <StyledTitle>{job?.title}</StyledTitle>
         <Card.Text>
-          {job?.company?.display_name} ({job?.location?.display_name})<br />
+          <MdLocationOn />
+          {job?.company?.display_name} ({job?.location?.display_name}) <br />
+          <BsFillCalendarDayFill />
           {moment(job?.created).format('DD MMM YYYY')} <br />
+          <FaBlackTie />
           {job?.description.slice(0, 200)}...
         </Card.Text>
         <StyledButton>Read More &rarr;</StyledButton>
